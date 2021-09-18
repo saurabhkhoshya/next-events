36069aa0febaae75c75d2d2f70a6287fda340f1e
unknown <saurabhkhoshya@gmail.com>
2021-09-18 23:22:24 +0530

    filter form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,14 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+import { Fragment } from "react";
 function AllEventPage() {
   const events = getAllEvents();
   return (
-    <div>
+    <Fragment>
+      <EventSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 
